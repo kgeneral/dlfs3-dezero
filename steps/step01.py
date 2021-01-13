5c83dc930917cc2aafd53ed25bcb48b0d55ddd50
Bruce Daeyeong Kim <bruce.daeyeong.kim@gmail.com>
2021-01-13 23:55:37 +0900

    step01 - fix stdout print, test member field 'ndim'

--- steps/step01.py
+++ steps/step01.py
@@ -2,10 +2,23 @@ class Variable:
     def __init__(self, data):
         self.data = data
 
-    def __str__(self) -> str:
-        return str(data)
 
 import numpy as np
+
 data = np.array(1.0)
 x = Variable(data)
-print(x)
+print(x.data)
+
+x.data = np.array(2.0)
+print(x.data)
+
+# ndim = number of dimensions
+y = np.array(1)
+print(y.ndim)
+
+y = np.array([1, 2, 3])
+print(y.ndim)
+
+y = np.array([[1, 2, 3],
+              [4, 5, 6]])
+print(y.ndim)
