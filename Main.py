1d41289bd872a3fc732370e8fe747f8a2e96f7b0
Bruce Daeyeong Kim <bruce.daeyeong.kim@gmail.com>
2021-01-21 22:18:11 +0900

    step02 - test code for Function class

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+from lib.Function import Function
 from lib.Variable import Variable
 
 # variable
+print("\n---variable---\n")
+
 data = np.array(1.0)
 x = Variable(data)
 print(x.data)
@@ -20,3 +23,13 @@ print(y.ndim)
 y = np.array([[1, 2, 3],
               [4, 5, 6]])
 print(y.ndim)
+
+# function
+print("\n---function---\n")
+
+x = Variable(np.array(10))
+f = Function()
+y = f(x)
+
+print(type(y))
+print(y.data)
