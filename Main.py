41fa335459ce27bee8c85c4c165305277e89fe7a
Bruce Daeyeong Kim <bruce.daeyeong.kim@gmail.com>
2021-01-22 09:06:07 +0900

    step07 - describe blueprint code for the recursive approach

--- Main.py
+++ Main.py
@@ -86,3 +86,36 @@ b.grad = C.backward(y.grad)
 a.grad = B.backward(b.grad)
 x.grad = A.backward(a.grad)
 print(x.grad)
+
+# backward with imperative
+print("\n---backward with imperative---\n")
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+assert y.creator == C
+assert y.creator.input == b
+assert y.creator.input.creator == B
+assert y.creator.input.creator.input == a
+assert y.creator.input.creator.input.creator == A
+assert y.creator.input.creator.input.creator.input == x
+
+y.grad = np.array(1.0)
+C = y.creator
+b = C.input
+b.grad = C.backward(y.grad)
+
+B = b.creator
+a = B.input
+a.grad = B.backward(b.grad)
+
+A = a.creator
+x = A.input
+x.grad = A.backward(a.grad)
+print(x.grad)
