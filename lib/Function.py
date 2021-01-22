764fde7833055df0182b0863937ee84aa9286f7c
Bruce Daeyeong Kim <bruce.daeyeong.kim@gmail.com>
2021-01-22 09:03:06 +0900

    step07 - make creator reference into Variable

--- lib/Function.py
+++ lib/Function.py
@@ -8,7 +8,9 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input  # memorize input variable
+        self.output = output  # memorize output
         return output
 
     def forward(self, x: np.ndarray) -> np.ndarray:
--- lib/Variable.py
+++ lib/Variable.py
@@ -5,3 +5,8 @@ class Variable:
     def __init__(self, data: np.ndarray):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
