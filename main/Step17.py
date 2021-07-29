369e8cf1429d03859b42edd2def3ec85464c526e
Bruce Daeyeong Kim <bruce.daeyeong.kim@gmail.com>
2021-07-29 22:10:50 +0900

    step17 - note for gc(for explanation)

---
+++ main/Step17.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from lib import add, square, exp
+from lib.Function import Function
+from lib.Variable import Variable
+
+print("\n---Garbage collection---\n")
+
+class Ref():
+    def __init__(self):
+        self.nextRef = []
+
+# 1 ref for each
+a = Ref()
+b = Ref()
+c = Ref()
+
+# b got 2 refs now
+a.nextRef = b
+# c got 2 refs now
+b.nextRef = c
+
+# a will be deleted
+# b also will be deleted : ref from a object has gone
+# c also will be deleted : ref from b object has gone
+a = b = c = None
